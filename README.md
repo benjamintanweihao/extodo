a66d28a9a3d8a6873bedfc03c68e808befa3549d
Benjamin Tan Wei Hao <benjamintanweihao@gmail.com>
2013-09-01 16:01:39 +0800

    completed Event module

--- README.md
+++ README.md
@@ -1,5 +1,55 @@
 # Extodo
 
-** Yet another TODO App … but, but in Elixir! **
+###Yet another TODO App … but, but in Elixir!
 
 This is basically a more or less faithful port the example application of Chapter 13 of Fred Hebert's excellent [Learn You Some Erlang for Great Good!](http://learnyousomeerlang.com/) book.
+
+Notes:
+
+Page 182:
+
+`iex -S mix`
+
+`spawn(Extodo.Event, :loop, [State[server: self, name: "test", to_go: 5]])`
+
+```
+iex> pid = spawn(Extodo.Event, :loop, [State[server: self, name: "test", to_go: 500]])
+#PID<0.106.0>
+iex> reply_ref = make_ref()
+#Reference<0.0.0.900>
+iex> pid <- { self, reply_ref, :cancel }
+{#PID<0.65.0>, #Reference<0.0.0.900>, :cancel}
+iex> flush
+{#Reference<0.0.0.900>, :ok}
+:ok
+```
+
+* Check out what are references
+* One reason we are using references is to make sure that we are receiving the response from the correct process/
+
+Page 185:
+
+```
+iex> c("lib/extodo/event.ex")
+Extodo.Event
+[Extodo.Event, State]
+iex> Extodo.Event.start("Event", 0)
+#PID<0.211.0>
+iex> flush()
+{:done, "Event"}
+:ok
+iex> pid = Extodo.Event.start("Event", 0)
+#PID<0.76.0>
+iex> Extodo.Event.cancel(pid)
+:ok
+```
+
+Page 186
+
+```
+iex> pid = Extodo.Event.start("Drink Beer", :calendar.local_time)
+#PID<0.105.0>
+iex> flush
+{:done, "Drink Beer"}
+:ok
+```
---
+++ lib/extodo/event.ex
@@ -0,0 +1,74 @@
+defrecord State, server: nil, name: "", to_go: 0
+
+defmodule Extodo.Event do
+  #############
+  # INTERFACE #
+  #############
+
+  def start(event_name, delay) do
+    spawn(__MODULE__, :init, [self, event_name, delay])
+  end
+
+  def start_link(event_name, delay) do
+    spawn_link(__MODULE__, :init, [self, event_name, delay])
+  end
+
+  # event's innards
+  def init(server, event_name, date_time) do
+    loop(State[server: server, name: event_name, to_go: time_to_go(date_time)])
+  end
+
+  def cancel(pid) do
+    # monitor in case the process is already dead.
+
+    # NOTE:
+    # We are using monitor to check if the process is already dead.
+    # If it is, we simply return ok. 
+    # Otherwise, the ref will be returned.
+    ref = Process.monitor(pid)
+    pid <- { self, ref, :cancel }
+
+    receive do
+      { ref, :ok } -> 
+        Process.demonitor(ref, [:flush])
+        :ok
+      { :DOWN, ref, :process, pid, _reason } ->
+        :ok
+    end
+  end
+
+  # NOTE: We need State[server: server] because we want to pattern match
+  #       `server` in the 'after' block.
+  def loop(state = State[server: server, to_go: [t|next]]) do
+    receive do
+      { server, ref, :cancel } -> 
+        server <- { ref, :ok } 
+    after t*1000 ->
+      case next do
+        [] -> 
+          server <- { :done, state.name }
+        _ ->
+          loop(State[to_go: next]) 
+      end
+    end
+  end
+
+  def time_to_go(time_out = {{_,_,_}, {_,_,_}}) do
+    now   = :calendar.local_time
+    to_go = :calendar.datetime_to_gregorian_seconds(time_out) - 
+            :calendar.datetime_to_gregorian_seconds(now)
+    secs  = if to_go > 0 do
+              to_go
+            else
+              0
+            end
+    normalize(secs)
+  end
+
+  def normalize(n) do
+    limit = 49*24*60*60
+    [rem(n, limit) | List.duplicate(limit, div(n, limit))]
+  end
+  
+end
+
